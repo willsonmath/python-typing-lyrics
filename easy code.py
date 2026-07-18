4f45196467da99cc5058e2d0daaa820895958501
Willson Andre Simamora <willsonandresimamora14@gmail.com>
2026-07-18 18:06:46 +0700

    Update comments and provide example usage in English
    Removed comments in Indonesian and added English example.

--- easy code.py
+++ easy code.py
@@ -7,8 +7,6 @@ Created : July 2026
 Description:
 Simple Python utility to create a typing effect for song lyrics.
 """
-#kode untuk mempermudah membuat efek ketikan lirik lagu
-
 import time
 def ketik(teks, waktu, jeda):
     if not teks:
@@ -22,10 +20,6 @@ def ketik(teks, waktu, jeda):
         print()
         time.sleep(jeda)
 
-#contoh;
+#Example;
 ketik("willson andre simamora", 5, 2)
 ketik("STEI-R ITB 26", 3, 0)
-#nanti tinggal ganti kata²nya jadi lirik lagu nya
-#angka nya itu kan ada dua, satu untuk "berapa detik satu baris itu dinyanyikan" dan satunya lagi untuk "berapa detik jeda dari baris satu ke baris selanjutnya"
-
-#selamat mencoba...
