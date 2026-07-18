89836ba70f56645280cf1edd085ca6e4152faea2
Willson Andre Simamora <willsonandresimamora14@gmail.com>
2026-07-18 18:24:28 +0700

    Refactor typing function and update documentation

--- easy code.py
+++ easy code.py
@@ -1,25 +1,24 @@
-"""
-Project : Python Typing Lyrics
-Version : 1.0
-Author  : Willson Andre Simamora
-Created : July 2026
-
-Description:
-Simple Python utility to create a typing effect for song lyrics.
-"""
 import time
-def ketik(teks, waktu, jeda):
-    if not teks:
+def typing_effect(text, duration, delay):
+    """
+    Display text with a typing animation.
+
+    Parameters:
+        text (str): Text to display.
+        duration (float): Total text typing duration.
+        delay (float): Pause before the next line.
+    """
+    if not text:
         print()
-        time.sleep(jeda)
+        time.sleep(delay)
     else:
-        speed = waktu/len(teks)
-        for i in range(1,len(teks)+1):
-            print(teks[:i],end="\r",flush=True)
+        speed = duration / len(text)
+        for i in range(1, len(text) + 1):
+            print(text[:i], end="\r", flush=True)
             time.sleep(speed)
         print()
-        time.sleep(jeda)
+        time.sleep(delay)
 
-#Example;
-ketik("willson andre simamora", 5, 2)
-ketik("STEI-R ITB 26", 3, 0)
+# Usage Example
+typing_effect("willson andre simamora", 5, 2)
+typing_effect("STEI-R ITB 26", 3, 0)
